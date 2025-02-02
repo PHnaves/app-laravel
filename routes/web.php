03b7b8f949c09e8117d26dcb5f2a55bfabfa3180
PHnaves <pnaves001@gmail.com>
2025-02-02 13:39:40 -0300

    adicao de novas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,40 @@
 <?php
 
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckIsAdmin;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users', [UserController::class, 'index'])->name('users');
+Route::middleware('auth')->group(function () {
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy')->middleware(CheckIsAdmin::class);
+    Route::get('/users/{user}/show', [UserController::class, 'show'])->name('users.show');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users', [UserController::class, 'index'])->name('users');
+
+    // rotas produtos
+    Route::get('/products', [ProductController::class, 'index'])->name('products');
+    Route::get('products/{product}/details', [ProductController::class, 'details'])->name('product.details');
+    Route::get('categories/{category}', [ProductController::class, 'categories'])->name('product.categories');
+    
+    Route::get('cart', [CartController::class, 'index'])->name('products.cart');
+    Route::post('add-to-cart', [CartController::class, 'AddToCart'])->name('product.Addcart');
+    Route::post('remove', [CartController::class, 'remove'])->name('product.removeCart');
+    Route::put('update', [CartController::class, 'update'])->name('product.updateCart');
+    Route::delete('clear-cart', [CartController::class, 'clear'])->name('product.clearCart');
+
+});
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 
 Route::get('/dashboard', function () {
